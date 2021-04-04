b635551ade5442fb477a6addb90778fa82acded2
Shivangi Srivastava <shi06dec@gmail.com>
2021-04-04 16:35:22 -0600

    Add article create and update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,36 @@ class ArticlesController < ApplicationController
   def index 
     @articles = Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create 
+    # render plain: params[:article]
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    # render plain: @article
+    # render plain: @article.inspect
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      # redirect_to article_path(@article)
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
 end
