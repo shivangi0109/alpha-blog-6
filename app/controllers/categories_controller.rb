379f3380a1d2c4530833f6f25fd5de6bd5b0b264
Shivangi Srivastava <shi06dec@gmail.com>
2021-04-14 16:19:36 -0600

    Add integration test and feature for listing categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,9 @@ class CategoriesController < ApplicationController
     @category = Category.new
   end
 
-  def index; end
+  def index
+    @categories = Category.paginate(page: params[:page], per_page: 5)
+  end
 
   def show
     @category = Category.find(params[:id])
