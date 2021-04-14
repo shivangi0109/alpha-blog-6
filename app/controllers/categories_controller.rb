90e501785c52f38183f49b69e4689e6b1841d866
Shivangi Srivastava <shi06dec@gmail.com>
2021-04-14 15:35:47 -0600

    Add integration test for create category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,7 +5,9 @@ class CategoriesController < ApplicationController
 
   def index; end
 
-  def show; end
+  def show
+    @category = Category.find(params[:id])
+  end
 
   def create 
     @category = Category.new(category_params)
