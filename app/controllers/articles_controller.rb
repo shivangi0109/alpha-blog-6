9c933d6d119e84c954f265eb552a8153424b6add
Shivangi Srivastava <shi06dec@gmail.com>
2021-04-19 11:42:38 -0600

    Update article and user views to display categories

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,7 +20,6 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    byebug
     # render plain: params[:article]
     @article = Article.new(article_params)
     # @article.user = User.first
